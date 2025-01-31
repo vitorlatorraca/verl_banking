14b933edb42f30a0c20e41668483fe2fb5c49c06
vitorlatorraca <vitorlatorraca5@gmail.com>
2025-01-31 00:01:53 -0500

    new

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,19 @@
+export const dynamic = 'force-dynamic'
+
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
-const ibmPlexSerif = IBM_Plex_Serif({ 
-  subsets: ["latin"],
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
   weight: ['400', '700'],
-  variable: '--font-ibm-plex-serif',
-  });
-
+  variable: '--font-ibm-plex-serif'
+})
 
 export const metadata: Metadata = {
   title: "Horizon",
-  description: "Horizon is a modern banking platform for everyone",
+  description: "Horizon is a modern banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
   }
